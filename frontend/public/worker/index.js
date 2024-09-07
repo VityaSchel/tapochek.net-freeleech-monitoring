0ed22cbcb6396fa27ff6d49826467cb6b8c54a12
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-09-07 20:17:05 +0400

    Add notification error handler

--- frontend/public/worker/index.js
+++ frontend/public/worker/index.js
@@ -2,15 +2,19 @@
 
 self.addEventListener('push', function (event) {
   const data = JSON.parse(event.data.text())
-  event.waitUntil(
-    registration.showNotification(data.title, {
-      body: data.message,
-      icon: 'https://tapochek.utidteam.com/android-chrome-192x192.png',
-      badge: 'https://tapochek.utidteam.com/safari-pinned-tab.svg',
-      data: { url: data.url },
-      image: data.image,
-    })
-  )
+  try {
+    event.waitUntil(
+      registration.showNotification(data.title, {
+        body: data.message,
+        icon: 'https://tapochek.utidteam.com/android-chrome-192x192.png',
+        badge: 'https://tapochek.utidteam.com/safari-pinned-tab.svg',
+        data: { url: data.url },
+        image: data.image,
+      })
+    )
+  } catch(e) {
+    console.log('Could not show notification', e)
+  }
 })
 
 self.addEventListener('notificationclick', function (event) {
